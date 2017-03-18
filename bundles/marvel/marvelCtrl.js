5686dd5763e691f14f8ea7c2100a5972c33ffb08
simon_tannai <simon.tannai@keyrus.com>
2017-03-18 20:06:21 +0100

    Characters API

--- bundles/marvel/marvelCtrl.js
+++ bundles/marvel/marvelCtrl.js
@@ -19,43 +19,86 @@ module.exports = class MarvelCtrl extends ClassCore {
     this._config = require(this.path.join(__dirname, '..', '..', 'config', 'marvelApi.json'))
     this._crypto = require('crypto')
     this._http = require('http')
-
-    this.testMarvelAPI()
   }
 
-  testMarvelAPI () {
-    const ts = Date.now()
-    const hash = this.crypto.createHash('md5').update(ts + this.config.privateKey + this.config.publicKey).digest('hex')
+  /**
+   * Get characters from marvel API
+   * @param {Number} offset   Row where results start. Default 0.
+   * @param {Number} limit    Number of results. Default 20.
+   * @return {Object}         Return JSON who contains characters
+   */
+  getCharacters (offset = 0, limit = 20) {
+    return new Promise((resolve, reject) => {
+      /**
+       * Current unix timestamp
+       * @type {Number}
+       */
+      const ts = Date.now()
 
-    const options = {
-      hostname: 'gateway.marvel.com',
-      port: 80,
-      path: `/v1/public/characters?ts=${ts}&apikey=${this.config.publicKey}&hash=${hash}`,
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/JSON'
+      /**
+       * MD5 hash
+       * @type {String}
+       */
+      const hash = this.crypto.createHash('md5').update(ts + this.config.privateKey + this.config.publicKey).digest('hex')
+
+      /**
+       * The path of the request
+       * @type {String}
+       */
+      let path = `/v1/public/characters?ts=${ts}&apikey=${this.config.publicKey}&hash=${hash}`
+
+      if (offset > 0) {
+        path += `&offset=${offset}`
       }
-    }
 
-    let chunks
+      if (limit > 0) {
+        path += `&limit=${limit}`
+      }
 
-    const req = this.http.request(options, (res) => {
-      res.setEncoding('utf8')
+      /**
+       * HTTP request's options
+       * @type {Object}
+       */
+      const options = {
+        hostname: 'gateway.marvel.com',
+        port: 80,
+        path: path,
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/JSON'
+        }
+      }
 
-      res.on('data', (chunk) => {
-        chunks += chunk
+      /**
+       * Full response's chunks
+       * @type {String}
+       */
+      let chunks = ''
+
+      /**
+       * HTTP request object
+       * @type {Object}
+       */
+      const req = this.http.request(options, (res) => {
+        res.setEncoding('utf8')
+
+        res.on('data', (chunk) => {
+          chunks += chunk
+        })
+
+        res.on('end', () => {
+          // Return results when request is done
+          return resolve(JSON.parse(chunks).data.results)
+        })
       })
 
-      res.on('end', () => {
-        console.log(chunks)
+      req.on('error', (e) => {
+        this.logger.error(e)
+        return reject(e)
       })
-    })
 
-    req.on('error', (e) => {
-      console.log(`problem with request: ${e.message}`)
+      req.end()
     })
-
-    req.end()
   }
 
   get config () {
--- bundles/marvel/marvelRouter.js
+++ bundles/marvel/marvelRouter.js
@@ -20,16 +20,16 @@ const logger = require(path.join(__dirname, '..', '..', 'core', 'Logger.js'))
  * Example route
  * URI: POST /
  */
-router.post('/', (req, res) => {
-  if (!req.body.query) {
-    logger.error('No query')
-
-    return res.status(403).send()
-  } else {
-    let someVar = exampleController.someFunction()
-
-    res.status(200).send(someVar)
-  }
+router.get('/characters/:offset/:limit', (req, res) => {
+  marvelCtrl.getCharacters(parseInt(req.params.offset), parseInt(req.params.limit))
+  .then(
+    (characters) => {
+      return res.status(200).json(characters)
+    },
+    (error) => {
+      return res.status(400).send(error)
+    }
+  )
 })
 
 /**
